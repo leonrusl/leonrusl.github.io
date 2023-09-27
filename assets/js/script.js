bb5d6ced7fdae5cddeb582708168c7bb3b1205d1
leonrusl <leonruslie00@gmail.com>
2023-09-27 08:36:55 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+ScrollReveal({
+    reset: false ,
+    distance: "60px",
+    duration: 2500,
+    delay: 100
+});
+
+
+// ScrollReveal().reveal(".container-fluid .navbar-nav", {delay: 500, origin: "top"});
+// ScrollReveal().reveal(".container-fluid .navbar-brand", {delay: 750, origin: "top"});
+// ScrollReveal().reveal("nav", {delay: 150, origin: "top"});
+// ScrollReveal().reveal("footer", {delay: 150, origin: "bottom"});
+// ScrollReveal().reveal("body", {delay: 150, origin: "bottom"});
+// ScrollReveal().reveal(".col-4 .card, menu1", {delay: 500, origin: "left"});
+// ScrollReveal().reveal(".col-6 .card, .datid", {delay: 350, origin: "right"});
+// ScrollReveal().reveal(".col-6 .asy .card", {delay: 350, origin: "bottom"});
